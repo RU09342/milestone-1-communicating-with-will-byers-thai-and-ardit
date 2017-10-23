206284f6a2e93baaa8bee4f15691374a9a45f20b
nghiemthai1 <nghiemthai1@gmail.com>
2017-10-22 21:21:11 -0400

    Update msp430fr599x_1.c

--- Milestone_StrangerThings/FR5994_mileStone/msp430fr599x_1.c
+++ Milestone_StrangerThings/FR5994_mileStone/msp430fr599x_1.c
@@ -22,11 +22,14 @@
 
 int TimerCount = 0;
 unsigned int byteCount = 0;
-unsigned int numOfBytes = 0;
+unsigned int numOfBytes = 0; //Maximum amount of Bytes
 volatile unsigned int i = 0;
 int redPWM, greenPWM, bluePWM;
-char Message[80];
-//UART Message;
+char Message[80];  //UART Message;
+
+void uartInitial(void);
+void ledinitial (void);
+void timerIntitial(void);
 
 int main(void)
 {
@@ -35,47 +38,12 @@ int main(void)
     PM5CTL0 &= ~LOCKLPM5;                    // Disable the GPIO power-on default high-impedance mode
                                             // to activate previously configured port settings
 
-    P2SEL0 &= ~(BIT0 | BIT1);                // PWM output to LED Port 2
-    P2SEL1 |= BIT0 | BIT1;
+    uartInitial();
+    ledInitial();
+    timerInitial();
 
-    // USCI_A0 UART operation and intialization (inspired by William Goh in his/her code of
-    //                    "eUSCI_A0 UART echo at 9600 baud"
-       CSCTL0_H = CSKEY_H;                     // Unlock CS registers
-       CSCTL1 = DCOFSEL_3 | DCORSEL;           // Set DCO to 8MHz
-       CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
-       CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers
-       CSCTL0_H = 0;                           // Lock CS registers
-       UCA0CTLW0 = UCSWRST;                    // Put eUSCI in reset
-       UCA0CTLW0 |= UCSSEL__SMCLK;             // CLK = SMCLK
-       UCA0BRW = 52;                           // 8000000/16/9600
-       UCA0MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
-       UCA0CTLW0 &= ~UCSWRST;                  // Initialize eUSCI
-       UCA0IE |= UCRXIE;CSCTL0_H = CSKEY_H;
-    CSCTL1 = DCOFSEL_3 | DCORSEL;
-    CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
-    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;
-    CSCTL0_H = 0;
-    UCA0CTLW0 = UCSWRST;
-    UCA0CTLW0 |= UCSSEL__SMCLK;
-    UCA0BRW = 52;
-    UCA0MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
-    UCA0CTLW0 &= ~UCSWRST;
-    UCA0IE |= UCRXIE;
 
-    P1DIR |= BIT0;        // Set P1.0 to output direction
-    P1OUT &= ~BIT0;       // Switch LED off
-
-    P1DIR |= BIT1;       //set Port 1.1 output ---LED
-    P1OUT &= ~BIT1;       // Swich LED off
-
-    P1DIR |= BIT3;       // P1.3 to output
-    P1DIR |= BIT4;     // P1.4 to output
-    P1DIR |= BIT5;      // P1.5 to output
-
-    TA0CCTL0 = (CCIE); //Timer A capture/control Interrupt Enable
-    TA0CCR0 = 0x0001; // Initialize CCRO
-    TA0CTL = TASSEL_2 + MC_1 + ID_3; // SMCLK / Upmode / Divider of 8
-
+    //Initialize PWM for each LEDs
     redPWM = 1;
     greenPWM = 1;
     bluePWM = 1;
@@ -85,56 +53,85 @@ int main(void)
 
 }
 
+// USCI_A0 UART operation and intialization (inspired by William Goh in his/her code of
+//                    "eUSCI_A0 UART echo at 9600 baud"
+void uartInitial(void){
+    CSCTL0_H = CSKEY_H;                     // Unlock CS registers
+    CSCTL1 = DCOFSEL_3 | DCORSEL;           // Set DCO to 8MHz
+    CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
+    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers
+    CSCTL0_H = 0;                           // Lock CS registers
+    UCA0CTLW0 = UCSWRST;                    // Put eUSCI in reset
+    UCA0CTLW0 |= UCSSEL__SMCLK;             // CLK = SMCLK
+    UCA0BRW = 52;                           // 8000000/16/9600
+    UCA0MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
+    UCA0CTLW0 &= ~UCSWRST;                  // Initialize eUSCI
+    UCA0IE |= UCRXIE;CSCTL0_H = CSKEY_H;
+}
 
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+void ledInitial(void){
+    P2SEL0 &= ~(BIT0 | BIT1);                // PWM output to LED Port 2
+    P2SEL1 |= BIT0 | BIT1;
+
+    P1DIR |= BIT0;        // Set P1.0 to output direction
+    P1OUT &= ~BIT0;       // Switch LED off
+
+    P1DIR |= BIT1;       //set Port 1.1 output ---LED
+    P1OUT &= ~BIT1;       // Swich LED off
+
+    P1DIR |= BIT3;       // P1.3 to output for Red LED
+    P1DIR |= BIT4;     // P1.4 to output for Green LED
+    P1DIR |= BIT5;      // P1.5 to output for Blue LED
+}
+
+void timerInitial(void){
+    TA0CCTL0 = (CCIE); //Timer A capture/control Interrupt Enable
+    TA0CCR0 = 0x0001; // Initialize CCRO
+    TA0CTL = TASSEL_2 + MC_1 + ID_3; // SMCLK / Upmode / Divider of 8
+}
+
+//uart interrupt vector
 #pragma vector=USCI_A0_VECTOR
 __interrupt void USCI_A0_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(USCI_A0_VECTOR))) USCI_A0_ISR (void)
-#else
-#error Compiler not supported!
-#endif
 {
     switch (__even_in_range(UCA0IV, USCI_UART_UCTXCPTIFG))
     {
     case USCI_NONE:
         break;
     case USCI_UART_UCRXIFG:
-        if (byteCount == 0)
+        if (byteCount == 0) //First message define maximum amount of bytes
         {
-            P1OUT |= BIT0
-            ;
-            numOfBytes = UCA0RXBUF;
-            byteCount++;
+            P1OUT |= BIT0; // Turn on the P1.0 LED
+            numOfBytes = UCA0RXBUF; // Message length going to be recieved
+            byteCount++; // Increment to get the next message
         }
         else if ((byteCount > 0) && (byteCount < 4))
         {
             switch (byteCount)
             {
             case 1:
-                redPWM = UCA0RXBUF;
+                redPWM = UCA0RXBUF; //get the value for red PWM from Buffer
                 break;
             case 2:
-                greenPWM = UCA0RXBUF;
+                greenPWM = UCA0RXBUF; //get the value for green PWM from Buffer
                 break;
             case 3:
-                bluePWM = UCA0RXBUF;
+                bluePWM = UCA0RXBUF; //get the value for blue PWM from Buffer
                 break;
 
             }
-            byteCount++;
+            byteCount++; // Increment to get the next message
         }
         else if ((byteCount > 3) && (byteCount < numOfBytes))
         {
-            Message[byteCount + 1] = UCA0RXBUF;
-            UCA0TXBUF = UCA0RXBUF;
-            byteCount++;
+            Message[byteCount + 1] = UCA0RXBUF; // Store the message
+            UCA0TXBUF = UCA0RXBUF; // Transmit the message
+            byteCount++; // Increment to get the next message
         }
         else if (byteCount >= numOfBytes)
         {
-            byteCount = 0;
-            P1OUT &= ~BIT0
-            ;
+            byteCount = 0; // Reset to prepare for the next message
+            P1OUT &= ~BIT0; // Turn off the P1.0 LED
         }
         __no_operation();
         break;
@@ -173,3 +170,4 @@ __interrupt void Timer0_A0_ISR(void)
     TimerCount++;
     TA0CCTL0 &= ~BIT0;  //clears BIT0
 }
+
